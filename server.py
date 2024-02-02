1364d79d1480eac06dde59996ca6d24afc2fc601
Toavina <126314831+Herinantenaina@users.noreply.github.com>
2024-02-02 09:03:13 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
 import requests
 import threading
-host = socket.gethostbyname(socket.gethostname())
+#host = socket.gethostbyname(socket.gethostname())
+host = '127.0.0.1'
 print(host)
 port = 8080
 
@@ -11,31 +12,72 @@ def start():
     server.listen(10)
     while True:
         try:
-            threading.Thread(target=handle_client, args=(server,)).start()
+            client_socket, client_addr = server.accept()
+            threading.Thread(target=handle_client, args=(client_socket,)).start()
         except ConnectionResetError:
-            print("ERROR")
+            print("[ERROR] Connection reset")
 
 
-def handle_client(server):
+def handle_client(client_socket):
     try:
-        client_socket, client_addr = server.accept()
+        # client_socket.setblocking(False)
         print(f"Client is connected at: {client_addr[0]}:{port}")
+        data = b''
         while True:
             message = client_socket.recv(1024)
         
             if not message:
                 break
-
-            message_ = message.decode()
+            #------Obtention du port, du methode et de la adresse host------
+            request = data + message
+            message_ = message.decode('utf-8')
             message_ = message_.split("\n")
-            message_ = message_[0]
-            _,message_,__ = message_.split()
-            message_,_ = message_.split(':')
-            print(message_)
             
+            #-----Method-----
+            method = message_[0]
+            method = method.split(" ")
+            method = method[0]
+            
+            #----Host_Web----
+            host_web = message_[1]
+            host_web = host_web.split(" ")
+            host_web = host_web[1]
+            host_web = host_web.split(":")
+            host_web = host_web[0]
+
+            #port_web
+            port_web = message_[0]
+            port_web = port_web.split(" ")
+            port_web = port_web[1]
+            port_web = port_web.split(":")  
+            if port_web[1] != '443':
+                port_web = 80
+            else:
+                port_web = int(port_web[1])
+            
+            print(f"Connecting to {host_web}:{port_web}")
+
+        #     #-----Connection du client au serveur cible-----
+        #     destination_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #     destination_server.connect((host_web, port_web))           
+            
+        #     #Send request to server
+        #     destination_server.sendall(request)
+            
+        #     #Receive data from destinatin server
+        #     while True:
+        #         response = destination_server.recv(1024)
+        #         print(f"[RESPONSE]{response.decode('utf-8')}")
+        #         #Check if data has a content
+        #         if len(data) > 0:
+        #             #send the response to the client
+        #             client_socket.send(response)
+        #         else:
+        #             break
+        # destination_server.close()
         client_socket.close()
     except ConnectionResetError:
-        print("[ERROR]")
+        print("[ERROR] Connection error")
         
 
 start()
