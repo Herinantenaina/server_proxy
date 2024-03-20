6aa3601f2164fb6cac41adb8ed78c0476ea6016b
Toavina <126314831+Herinantenaina@users.noreply.github.com>
2024-03-20 20:39:47 +0300

    Add files via upload

--- server/client.py
+++ server/client.py
@@ -1,22 +1,20 @@
 import socket
 import ssl
 
-host = 'localhost'
-port = 8080
+#host = 'localhost'
+host = '127.0.0.1'
+port = 8081
 
 def start():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket :
         client_socket.connect((host,port))
         # context = ssl.create_default_context()
         # client_socket = context.wrap_socket(client_socket, server_hostname= 'example.com')
-        #client_socket.sendall(b'GET http://example.com/ HTTP/1.1\r\nHost: example.com\r\nProxy-Connection: keep-alive\r\n\r\n')
-        try:
-            response = client_socket.recv(1024)
-            print(response.decode('utf-8'))
-        except:
-            print('No response')
-        finally:
-            client_socket.close()
+        client_socket.sendall(b'GET http://example.com/ HTTP/1.1\r\nHost: example.com\r\nProxy-Connection: keep-alive\r\n\r\n')
+        while True:  
+                response = client_socket.recv(1024)
+                print(response.decode('utf-8'))
+            
 
 if __name__ == '__main__':
     start()
