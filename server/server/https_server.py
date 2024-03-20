bd847076a75a1c93925fe449001ad6089e93cf8b
Toavina <126314831+Herinantenaina@users.noreply.github.com>
2024-03-20 20:40:50 +0300

    Add files via upload

--- server/server/https_server.py
+++ server/server/https_server.py
@@ -1,49 +1,16 @@
-import errno
 import socket
+import ssl
+import time
 import threading
 import signal
-import ssl
 
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+context.load_cert_chain(certfile='ssl/cert.pem', keyfile='ssl/key.pem')
+context.check_hostname = False
 host = 'localhost'
 port = 8081
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile='ssl/cert.pem', keyfile='ssl/key.pem')
+# hostname_server = socket.getfqdn()
 
-
-#------------Handling client----------------
-def handle_client(client_socket):
-    if client_socket.fileno() != -1: #  Check if the client's socket is closed
-        try:
-            # client_socket.setblocking(False)
-            while True:
-                try:
-                    message = client_socket.recv(1024) 
-                except Exception as e:
-                    print(e)
-                    break
-
-                if not message:
-                    break
-
-                #------Obtention du port, du methode et de l'adresse host------
-                host_web=  extract_port_host_method_request(message)
-                
-                # Correction du requête
-                if host_web == 'example.com' and host_web != None:
-                    message = _remove(message)
-                    try:
-                        handle_destination_server(host_web, client_socket, message)
-                    except Exception as e:
-                        print(e) 
-            
-            client_socket.close()
-        except ConnectionResetError:
-            print("[ERROR] Connection error")
-        finally:
-            client_socket.close()
-
-
-#-------------METHOD-PORT-HOST--------------
 def extract_port_host_method_request(message):
     try:
         message_ = _decode(message)
@@ -64,46 +31,9 @@ def extract_port_host_method_request(message):
 
         return host_web
     except:
-        return None,
-
-        
-#-----Connection du client au serveur cible-----
-def handle_destination_server(host_web, client_socket, message):
-    print(f'Connecting to: {host_web}')
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as destination_server:
-            destination_server.connect((host_web, 80))
-            destination_server.sendall(message)
-            
-            while True:
-                    server_response = destination_server.recv(1024)
-                    if len(server_response) > 0:
-                        client_socket.sendall(server_response)
-                    else:
-                        break 
-                    
-            destination_server.close()
-    except Exception as e:
-       print(e,'[Fermé par le serveur]')
-    finally:
-        client_socket.close()
-        destination_server.close()
-
-#-------------Remove the error int the request due to the domain being wronged-----------
-def _remove(message:bytes):
-    message = message.decode('utf-8')
-    message = message.split(' ')
-    message[1] = '/'
-    message = ' '.join(message)
-    message = b'' + message.encode()
-    return message
-
-#------------Stopping the server manually-----------------
-def signal_handler(signal, frame):
-    print('[SERVER] Stopping the server...')
-    exit(0)
+        return None
     
-#-------------Decoding the message since some have different format--------------
+#-------------Decoding the message since some have different format--------------    
 def _decode(message:any):
     try:
         message = message.decode('utf-8')
@@ -117,32 +47,85 @@ def _decode(message:any):
                 message = message.decode('Windows-1252')
                 return message
             except:
-                return None   
-    
-           
+                return None 
+
+#-------------Remove the error int the request due to the domain being wronged-----------
+def _remove(message:bytes):
+    message = message.decode('utf-8')
+    message = message.split(' ')
+    message[1] = '/'
+    message = ' '.join(message)
+    message = b'' + message.encode()
+    return message
+
+
+#------------Stopping the server manually-----------------
+def signal_handler(signal, frame):
+    print('[SERVER] Stopping the server...')
+    exit(0)
+
+
+def request(client_socket):
+    if client_socket.fileno() != -1:
+        try:
+            while True:
+                try:
+                    message = client_socket.recv(1024)
+                except Exception as e:
+                    break
+
+                if not message:
+                    break
+ 
+                #------Obtention du port, du methode et de l'adresse host------
+                host_web =  extract_port_host_method_request(message)
+                if host_web == 'example.com' and host_web != None:
+                    print(message)
+                    message = _remove(message)
+                    print('A client is connected')
+                    with socket.create_connection((host_web, 443)) as sock:
+                        with context.wrap_socket(sock, server_hostname=host_web) as ssock:
+                            ssock.send(message)
+                            response = bytes()
+                            while True:
+                                try:
+                                    response= ssock.recv(1024)
+                                    message = client_socket.recv(1024)
+                                    print(message)
+                                    client_socket.sendall(response)
+                                    print(response)
+                                except Exception as e:
+                                    print(e)
+                                    break
+                                finally:
+                                    sock.close()
+        except ConnectionError:
+            print('Connection error')
+        finally:
+            client_socket.close()   
+
+
 #-------------Starting proxy------------------
 def start():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-    server.bind((host,port))
-    server.listen(5)
-    
-    print('[SERVER]  The server is on...')
-    while True:
-        try:
-            client_socket, client_addr = server.accept()
-            # Wrapping the client_socket to use ssl/tsl encryption
-            client_socket = context.wrap_socket(client_socket, server_side= True)
-            threading.Thread(target=handle_client, args=(client_socket,), daemon=True).start()
-            signal.signal(signal.SIGINT, signal_handler)
-        except ConnectionResetError:
-            print("[ERROR] Connection reset")
-        except OSError as e :
-            if e.errno != errno.EINTR:
-                raise
-        except KeyboardInterrupt :
-            print('[SERVER] The server is stopping...')
-            exit(0)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+        server.bind((host,port))
+        server.listen(5)
+        print('[SERVER]  The server is on...')
+        # server = context.wrap_socket(server, server_side=True)
+        while True:
+            try:
+                client_socket, client_addr = server.accept()
+                # client_socket = context.wrap_socket(client_socket, server_side=True)
+                threading.Thread(target=request, args=(client_socket,), daemon=True).start()
+                signal.signal(signal.SIGINT, signal_handler)
+            except ConnectionResetError:
+                print("[ERROR] Connection reset")
+            except OSError as e :
+                raise 
+            except KeyboardInterrupt:
+                print('[SERVER] The server is stopping...')
+                exit(0)
 
 
 start()
