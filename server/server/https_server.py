fbf9c5b610869991787df435ba655bfce2c496e9
Toavina <126314831+Herinantenaina@users.noreply.github.com>
2024-03-25 15:59:55 +0300

    Add files via upload

--- server/server/https_server.py
+++ server/server/https_server.py
@@ -4,11 +4,24 @@ import time
 import threading
 import signal
 
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-context.load_cert_chain(certfile='ssl/cert.pem', keyfile='ssl/key.pem')
-context.check_hostname = False
+
+#----------Web context-----------
+context_web = ssl.create_default_context()
+context_web.load_verify_locations('ssl/ca.pem')
+
+#---------Client context---------
+context_client = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context_client.load_cert_chain(certfile='ssl/cert.pem', keyfile='ssl/key.pem')
+context_client.load_verify_locations(cafile='ssl/ca.pem')
+
+#----------Server context--------
+context_server = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context_server.load_cert_chain(certfile='ssl/cert.pem', keyfile='ssl/key.pem')
+context_server.check_hostname = True
+context_server.verify_mode = ssl.CERT_REQUIRED
+
 host = 'localhost'
-port = 8081
+port = 8080
 # hostname_server = socket.getfqdn()
 
 def extract_port_host_method_request(message):
@@ -26,8 +39,8 @@ def extract_port_host_method_request(message):
         try:
             ip_web = f"'{socket.gethostbyname(host_web)}'"
         except:
-            print("Wrong domain name entered: [",host_web,"]" )     
-    
+            print("Wrong domain name entered: [",host_web,"]" )   
+            host_web = None
 
         return host_web
     except:
@@ -53,6 +66,7 @@ def _decode(message:any):
 def _remove(message:bytes):
     message = message.decode('utf-8')
     message = message.split(' ')
+    message[0] = 'GET'
     message[1] = '/'
     message = ' '.join(message)
     message = b'' + message.encode()
@@ -65,44 +79,60 @@ def signal_handler(signal, frame):
     exit(0)
 
 
-def request(client_socket):
-    if client_socket.fileno() != -1:
+def request(_client_socket):
+    if _client_socket.fileno() != -1:
         try:
             while True:
                 try:
-                    message = client_socket.recv(1024)
+                    message = _client_socket.recv(1024)
                 except Exception as e:
+                    print(e)
                     break
-
+                
                 if not message:
                     break
- 
+                
                 #------Obtention du port, du methode et de l'adresse host------
                 host_web =  extract_port_host_method_request(message)
                 if host_web == 'example.com' and host_web != None:
-                    print(message)
                     message = _remove(message)
                     print('A client is connected')
-                    with socket.create_connection((host_web, 443)) as sock:
-                        with context.wrap_socket(sock, server_hostname=host_web) as ssock:
-                            ssock.send(message)
-                            response = bytes()
+                    
+                    with socket.create_connection((host_web, 443)) as web:
+                        with context_web.wrap_socket(web, server_hostname=host_web) as secure_web:
+                            _client_socket.sendall(b'HTTP/1.1 200 Connection Established\r\n\r\n')
+                            client_socket = context_client.wrap_socket(_client_socket, server_side=True, do_handshake_on_connect=False)
+                            try:
+                                print('Performing handshake')
+                                client_socket.do_handshake()
+                            except Exception as e:
+                                print(e,'+++++++++++')
+                                print('Handshake failed')
+                            data = client_socket.recv(4096)
+                            print(data)
+                            secure_web.sendall(message)
                             while True:
                                 try:
-                                    response= ssock.recv(1024)
-                                    message = client_socket.recv(1024)
-                                    print(message)
+                                    response= secure_web.recv(4096)
+                                    if not response:
+                                        break
                                     client_socket.sendall(response)
                                     print(response)
                                 except Exception as e:
                                     print(e)
                                     break
                                 finally:
-                                    sock.close()
+                                    secure_web.shutdown(socket.SHUT_RDWR)
+                                    secure_web.close()
+                                    client_socket.shutdown(socket.SHUT_RDWR)
+                                    client_socket.close()
+            _client_socket.close()  
+
         except ConnectionError:
             print('Connection error')
         finally:
-            client_socket.close()   
+            _client_socket.shutdown(socket.SHUT_RDWR)
+            _client_socket.close()  
 
 
 #-------------Starting proxy------------------
@@ -111,21 +141,26 @@ def start():
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
         server.bind((host,port))
         server.listen(5)
+        # secure_server = context_server.wrap_socket(server, server_hostname='localhost')
+        # secure_server.bind((host,port))
+        # secure_server.listen(5)
         print('[SERVER]  The server is on...')
         # server = context.wrap_socket(server, server_side=True)
-        while True:
-            try:
-                client_socket, client_addr = server.accept()
-                # client_socket = context.wrap_socket(client_socket, server_side=True)
-                threading.Thread(target=request, args=(client_socket,), daemon=True).start()
-                signal.signal(signal.SIGINT, signal_handler)
-            except ConnectionResetError:
-                print("[ERROR] Connection reset")
-            except OSError as e :
-                raise 
-            except KeyboardInterrupt:
+        try:
+            signal.signal(signal.SIGINT, signal_handler)
+            while True:
+                try:
+                    try:
+                        _client_socket, client_addr = server.accept()
+                        threading.Thread(target=request, args=(_client_socket,), daemon=True).start()
+                    except Exception as e:
+                        print(e,'-------------------')
+                except ConnectionResetError:
+                    print("[ERROR] Connection reset")
+                except OSError as e :
+                    raise 
+        except KeyboardInterrupt:
                 print('[SERVER] The server is stopping...')
                 exit(0)
-
 
 start()
